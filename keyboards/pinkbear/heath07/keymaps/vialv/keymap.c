e6561b8d04060f7689668577c5f7e3b93cd0552e
Val Song <valsong@163.com>
2025-10-22 21:18:45 +0800

    增加heath07新pcb

---
+++ keyboards/pinkbear/heath07/keymaps/vialv/keymap.c
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_ESC,        KC_1,         KC_2,        KC_3,        KC_4,        KC_5,        KC_6,        KC_7,        KC_8,       KC_9,        KC_0,        KC_MINS,      KC_EQL,      KC_BSPC,            KC_HOME,        KC_PGUP,
+        KC_TAB,        KC_Q,         KC_W,        KC_E,        KC_R,        KC_T,        KC_Y,        KC_U,        KC_I,       KC_O,        KC_P,        KC_LBRC,      KC_RBRC,     KC_BSLS,            KC_END,         KC_PGDN,
+        KC_LCTL,       KC_A,         KC_S,        KC_D,        KC_F,        KC_G,        KC_H,        KC_J,        KC_K,       KC_L,        KC_SCLN,     KC_QUOT,      KC_TRNS,     KC_ENT,             KC_TRNS,        KC_TRNS,
+        KC_LSFT,       KC_Z,         KC_X,        KC_C,        KC_V,        KC_B,        KC_N,        KC_M,        KC_COMM,    KC_DOT,      KC_SLSH,     _______,      _______,     KC_RSFT,            KC_UP,          _______,
+        MO(1),         KC_LALT,      KC_LGUI,     _______,     _______,     KC_SPC,      _______,     _______,     _______,    KC_RALT,     MO(1),       MO(2),        _______,     KC_LEFT,            KC_DOWN,        KC_RGHT
+    ),
+
+     [1] = LAYOUT(
+        KC_GRV,        KC_F1,        KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,       KC_F7,       KC_F8,       KC_F9,      KC_F10,       KC_F11,      KC_F12,      KC_DEL,             KC_TRNS,        KC_TRNS,
+        S(KC_GRV),     KC_TRNS,      KC_UP,       KC_TRNS,     G(KC_SPC),   KC_TRNS,     KC_INS,      KC_BSPC,     KC_DEL,      KC_ENT,     KC_PGUP,      KC_ESC,      KC_TRNS,     KC_TRNS,            KC_TRNS,        KC_TRNS,
+        KC_TRNS,       KC_LEFT,      KC_DOWN,     KC_RIGHT,    KC_TRNS,     KC_TRNS,     KC_LEFT,     KC_DOWN,     KC_UP,       KC_RGHT,    KC_PGDN,      KC_END,      KC_TRNS,     KC_TRNS,            KC_TRNS,        KC_TRNS,
+        KC_TRNS,       KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_HOME,     KC_PGDN,     KC_PGUP,     KC_END,     KC_TRNS,      _______,     _______,     KC_RSFT,            KC_UP,          _______,
+        KC_TRNS,       KC_TRNS,      KC_TRNS,     _______,     _______,     KC_TRNS,     _______,     _______,     _______,     KC_TRNS,    KC_TRNS,      KC_TRNS,     _______,     KC_LEFT,            KC_DOWN,        KC_RGHT
+    ),
+
+     [2] = LAYOUT(
+        KC_TRNS,       KC_BRID,      KC_BRIU,     KC_NO,       KC_NO,       RGB_VAD,     RGB_VAI,     KC_MPRV,     KC_MPLY,     KC_MNXT,    KC_MUTE,      KC_VOLD,      KC_VOLU,    LCA(KC_DEL),        KC_TRNS,        KC_TRNS,
+        RGB_TOG,       KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,      TO(4),        TO(3),      TO(0),              KC_TRNS,        KC_TRNS,
+        KC_CAPS,       RGB_MOD,      RGB_HUI,     RGB_SAI,     RGB_VAI,     RGB_SPI,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,      KC_TRNS,      KC_TRNS,    KC_TRNS,            KC_TRNS,        KC_TRNS,
+        GU_OFF,        RGB_RMOD,     RGB_HUD,     RGB_SAD,     RGB_VAD,     RGB_SPD,     NK_ON,       NK_OFF,      KC_TRNS,     KC_TRNS,    KC_TRNS,      _______,      _______,    KC_TRNS,            KC_TRNS,        _______,
+        AG_LSWP,       GU_ON,        AG_LNRM,     _______,     _______,     KC_TRNS,     _______,     _______,     _______,     KC_TRNS,    KC_TRNS,      KC_TRNS,      _______,    KC_TRNS,            KC_TRNS,        KC_TRNS
+        ),
+
+     [3] = LAYOUT(
+        TO(0),         KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_TRNS,            KC_TRNS,        KC_TRNS,
+        KC_TRNS,       KC_NO,        KC_MS_UP,    KC_NO,       KC_NO,       KC_NO,       KC_MS_WH_LEFT,KC_MS_WH_DOWN,KC_MS_WH_UP,KC_MS_WH_RIGHT,KC_MS_ACCEL2,KC_NO,     KC_NO,      KC_TRNS,            KC_TRNS,        KC_TRNS,
+        KC_TRNS,       KC_MS_LEFT,   KC_MS_DOWN,  KC_MS_RIGHT, KC_NO,       KC_NO,       KC_MS_BTN3,  KC_MS_BTN1,  KC_MS_BTN2,  KC_MS_ACCEL0,KC_MS_ACCEL1,KC_MS_ACCEL2, KC_TRNS,    KC_TRNS,            KC_TRNS,        KC_TRNS,
+        KC_TRNS,       KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,      KC_NO,        _______,      _______,    KC_TRNS,            KC_TRNS,        _______,
+        KC_TRNS,       KC_TRNS,      KC_TRNS,     _______,     _______,     KC_TRNS,     _______,     _______,     _______,     KC_TRNS,    KC_TRNS,      KC_TRNS,      _______,    KC_TRNS,            KC_TRNS,        KC_TRNS
+        ),
+
+     [4] = LAYOUT(
+        TO(0),         KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_NO,              KC_NO,          KC_NO,
+        KC_NO,         KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_NO,              KC_NO,          KC_NO,
+        KC_NO,         KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_NO,              KC_NO,          KC_NO,
+        KC_NO,         KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,      KC_NO,        _______,      _______,    KC_NO,              KC_NO,          _______,
+        KC_NO,         KC_NO,        KC_NO,       _______,     _______,       KC_NO,     _______,     _______,     _______,     KC_NO,      KC_NO,        KC_NO,        _______,    KC_NO,              KC_NO,          KC_NO
+        ),
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] =   { ENCODER_CCW_CW(RGB_SAD, RGB_SAI)  },
+    [2] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [3] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [4] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+};
+#endif
---
+++ keyboards/pinkbear/heath07/readme.md
@@ -0,0 +1,52 @@
+# Gray Studio bd60
+
+ A 60% keyboard by Graystudio. PCB designed and manufactured by DEMO Studio.
+
+ * Keyboard Maintainer: [edwardslau](https://github.com/edwardslau)
+ * Hardware Support: bd60
+ * Hardware Availability:
+
+ Make example for this keyboard (after setting up your build environment):
+
+ 	make pinkbear heath07:default
+
+ Flashing example for this keyboard:
+
+ 	make pinkbear heath07:default:flash
+
+## Bootloader
+
+* **Physical reset button**: Briefly press the button on the back of the PCB - some may have pads you must short instead
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more infromation. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
+
+# 初始化vial环境并检查  
+
+```shell
+make git-submodule
+qmk doctor
+```
+
+# 编译固件  
+
+```shell
+make pinkbear/heath07:vial
+make pinkbear/heath07:vialv
+```
+
+```shell
+qmk compile -kb pinkbear/heath07 -km vial
+qmk compile -kb pinkbear/heath07 -km vialv
+```
+
+# 刷新固件
+
+```shell
+make pinkbear/heath07:vialv:flash
+```
+
+```shell
+qmk flash -kb pinkbear/heath07 -km vialv
+```
+
